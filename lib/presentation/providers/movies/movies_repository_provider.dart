38844a0fff20dbbe02c3e386eef8d84d4ec0431b
Abraham De La Hoya <abrahamadlha@gmail.com>
2024-12-13 13:12:22 -0600

    First View Done

---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,9 @@
+import 'package:cinemapedia/infrestructure/datasource/moviedb_datasource.dart';
+import 'package:cinemapedia/infrestructure/repositories/movie_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Este repositorio es inmutable
+
+final movieRepositoryProvider = Provider((ref) {
+  return MovieRepositoryImpl( MoviedbDatasource() );
+});
